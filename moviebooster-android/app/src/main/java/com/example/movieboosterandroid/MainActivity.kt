15dadc9adab17fdd10f35a9260f699fcbe045b85
admin <admin@admins-iMac.local>
2022-08-14 08:38:07 +0300

    Added KMM to android

--- moviebooster-android/app/src/main/java/com/example/movieboosterandroid/MainActivity.kt
+++ moviebooster-android/app/src/main/java/com/example/movieboosterandroid/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.movieboosterandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.kmmshared.Greeting
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val greetingTextView = findViewById<TextView>(R.id.greeting_text_view)
+        greetingTextView.text = Greeting().greeting()
     }
 }
